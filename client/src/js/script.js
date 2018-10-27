0b9b3722a7e3e8e60d7cf346f783820ef20b3b01
Joran Rood <joran@rood.me>
2018-10-27 11:32:15 +0200

    Add the host and guest event listeners using a method
    Previously, these event listeners were added in the constructor

--- client/src/js/script.js
+++ client/src/js/script.js
@@ -1,52 +1,71 @@
 class HostController {
-  constructor(player) {
+  constructor(socket, player) {
+    this.socket = socket;
     this.player = player;
-    this.player.addEventListener('onReady', this.ready);
-    this.player.addEventListener('onStateChange', this.stateChange);
   }
 
-  ready() {
-    // Connect to the node server
-  }
+  initialise() {
+    this.socket.addEventListener('message', (event) => {
+      let error, message;
 
-  stateChange() {
-    //
-  }
+      try {
+        message = JSON.parse(event.data);
+      } catch (error) {
+        //
+      }
+    });
 
-  //
+    this.player.addEventListener('onStateChange', (event) => {
+      // Propagate state changes to the guests
+      this.socket.send(JSON.stringify({}));
+    });
+  }
 }
 
 class GuestController {
-  constructor(player) {
+  constructor(socket, player) {
+    this.socket = socket;
     this.player = player;
-    this.player.addEventListener('onReady', this.ready);
-    this.player.addEventListener('onStateChange', this.stateChange);
   }
 
-  ready() {
-    // Connect to the node server
-  }
+  initialise() {
+    this.socket.addEventListener('message', (event) => {
+      let error, message;
 
-  stateChange() {
+      try {
+        message = JSON.parse(event.data);
+        // Mimic host state changes
 
+      } catch (error) {
+        //
+      }
+    });
+
+    this.player.addEventListener('onStateChange', (event) => {
+      //
+    });
   }
 }
 
 (function() {
   // Load the YouTube iFrame API
   const scriptNode = document.createElement('script');
-  scriptNode.src = 'https://www.youtube.com/iframe_api';
+  scriptNode.src = 'http://www.youtube.com/iframe_api';
   document.body.appendChild(scriptNode);
 
   /* Instantiate the YouTube Player and the controller
      once the iFrame API has been loaded */
   window.onYouTubeIframeAPIReady = () => {
-    let player = new YT.Player('player', {
-      width: 1280, // 16
-      height: 720 // 9
+    const player = new YT.Player('player', {
+      width: 1280, // 16:
+      height: 720 // :9
     });
 
-    let controller = new HostController(player);
-    // let controller = new GuestController(player);
+    const socket = new WebSocket('ws://127.0.0.1:8081');
+    const controller = new HostController(socket, player);
+    // const controller = new GuestController(socket, player);
+
+    // TODO: Wait for the socket to be ready as well
+    player.addEventListener('onReady', () => controller.initialise());
   };
 })();
